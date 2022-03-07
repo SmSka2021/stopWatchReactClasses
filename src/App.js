e0a658d40e361f8069b648cacb329bccc99dc20b
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-03-07 17:35:12 +0300

    realis function

--- src/App.js
+++ src/App.js
@@ -1,97 +1,62 @@
-import React from "react";
-import { DisplayComponent } from "./Components/DisplayComponent";
-import { BtnComponent } from "./Components/BtnComponent";
-import "./App.css";
+import React, {useState} from 'react';
+import {DisplayComponent} from './Components/DisplayComponent';
+import {BtnComponent} from './Components/BtnComponent';
+import './App.css';
 
-export class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: {
-        ms: 0,
-        s: 0,
-        m: 0,
-        h: 0,
-      },
-      interv: "",
-      status: 0,
-    };
-    this.updatedMs = this.state.time.ms;
-    this.updatedS = this.state.time.s;
-    this.updatedM = this.state.time.m;
-    this.updatedH = this.state.time.h;
+export const App = () => {
+  const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+  // Not started = 0
+  // started = 1
+  // stopped = 2
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
 
-    this.start = this.start.bind(this);
-    this.run = this.run.bind(this);
-    this.stop = this.stop.bind(this);
-    this.reset = this.reset.bind(this);
-    this.resume = this.resume.bind(this);
-    this.reset_update = this.reset_update.bind(this);
-  }
-  reset_update(){
-    this.updatedMs = 0;
-    this.updatedS = 0;
-    this.updatedM = 0;
-    this.updatedH = 0;
-  }
-  start() {    
-    this.run();
-    this.setState({ status: 1 });
-    this.setState({ interv: setInterval(this.run, 10) });
-  }
+  let updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
 
-  run() {
-    if (this.updatedM === 60) {
-      this.updatedH++;
-      this.updatedM = 0;
+  const run = () => {
+    if(updatedM === 60){
+      updatedH++;
+      updatedM = 0;
     }
-    if (this.updatedS === 60) {
-      this.updatedM++;
-      this.updatedS = 0;
+    if(updatedS === 60){
+      updatedM++;
+      updatedS = 0;
     }
-    if (this.updatedMs === 100) {
-      this.updatedS++;
-      this.updatedMs = 0;
+    if(updatedMs === 100){
+      updatedS++;
+      updatedMs = 0;
     }
-    this.updatedMs++;
-    return this.setState({
-      time: {
-        ms: this.updatedMs,
-        s: this.updatedS,
-        m: this.updatedM,
-        h: this.updatedH,
-      },
-    });
-  }
-  stop() {
-    clearInterval(this.state.interv);
-    this.setState({ status: 2 });
-  }
-  reset() {
-    clearInterval(this.state.interv);
-    this.setState({ time: { ms: 0, s: 0, m: 0, h: 0 } });
-    this.setState({ status: 0 });
-    this.reset_update();    
-  }
-  resume() {
-    this.start();
-  }
-  render() {
-    return (
-      <div className="main_section">
-        <div className="clock_holder">
+    updatedMs++;
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0})
+  };
+
+  const resume = () => start();
+
+  return (
+    <div className="main_section">
+     <div className="clock_holder">
           <div className="stopwatch">
-            <DisplayComponent time={this.state.time} />
-            <BtnComponent
-              status={this.state.status}
-              resume={this.resume}
-              reset={this.reset}
-              stop={this.stop}
-              start={this.start}
-            />
+               <DisplayComponent time={time}/>
+               <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start}/>
           </div>
-        </div>
-      </div>
-    );
-  }
+     </div>
+    </div>
+  );
 }
+
